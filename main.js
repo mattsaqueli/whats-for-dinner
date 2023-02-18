3ceab7d73cf85de2dc5c378edc4136ed0bf35171
Matt Saqueli <msaqueli22@turing.edu>
2023-02-18 11:21:25 -0600

    Add dish variables, querySelectors, and getRandom function

---
+++ main.js
@@ -0,0 +1,63 @@
+var sides = [
+    "Miso Glazed Carrots",
+    "Coleslaw",
+    "Garden Salad",
+    "Crispy Potatoes",
+    "Sweet Potato Tots",
+    "Coconut Rice",
+    "Caeser Salad",
+    "Shrimp Summer Rolls",
+    "Garlic Butter Mushrooms",
+    "Hush Puppies"
+]
+
+var mains = [
+    "Spaghetti and Meatballs",
+    "Pineapple Chicken",
+    "Shakshuka",
+    "Thai Yellow Curry",
+    "Bibimbap",
+    "Chicken Parmesean",
+    "Butternut Squash Soup",
+    "BBQ Chicken Burgers",
+    "Ramen",
+    "Empanadas",
+    "Chicken Fried Rice",
+    "Sheet Pan Fajitas",
+    "Margarita Pizza"
+]
+
+var desserts = [
+    "Apple Pie",
+    "Lemon Meringue Pie",
+    "Black Forest Cake",
+    "Banana Bread",
+    "Peach Cobbler",
+    "Cheesecake",
+    "Funfetti Cake",
+    "Baklava",
+    "Flan",
+    "Macarons",
+    "Macaroons",
+    "Chocolate Cupcakes",
+    "Pavlova",
+    "Pumpkin Pie",
+    "Key Lime Pie",
+    "Tart Tatin",
+    "Croissants",
+    "Eclairs"
+]
+
+var selectionBtn = document.querySelector('.selection-btn');
+var letsCookBtn = document.querySelector('.lets-cook-btn');
+var clearBtn = document.querySelector('.clear-btn');
+var cookPotImg = document.querySelector('cookpot-img');
+var changeBox = document.querySelector('.change-box')
+
+letsCookBtn.addEventListener('click', randomRecipe);
+clearBtn.addEventListener('click', clearRecipe);
+
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
