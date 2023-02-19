eba1fa3e96f33a7553168967bcad50e0927cf32d
Matt Saqueli <msaqueli22@turing.edu>
2023-02-19 10:53:56 -0600

    Add onto the random recipe function for all meal options

--- main.js
+++ main.js
@@ -75,8 +75,42 @@ function randomRecipe() {
       var btnOptions = document.getElementsByName('radio');
       for (var i = 0; i < btnOptions.length; i++) {
           if (btnOptions[i].checked) {
+              btnOptions[i].checked = false;
               var mealOption = (btnOptions[i].value);
             }
         }
+    if (mealOption === 'side') {
+            changeBox.innerHTML = `
+        <div class='dish'>
+            <p class='you-should-make'>You should make:</p>
+            <p class='dish-name'><font size ='5'>${sides[getRandomIndex(sides)]}</p>
+        </div>
+     `
+    } else if (mealOption === 'main') {
+        changeBox.innerHTML = `
+        <div class='dish'>
+            <p class='you-should-make'>You should make:</p>
+            <p class='dish-name'><font size ='5'>${mains[getRandomIndex(mains)]}</p>
+        </div>
+     `
+    } else if (mealOption === 'dessert') {
+        changeBox.innerHTML = `
+        <div class='dish'>
+            <p class='you-should-make'>You should make:</p>
+            <p class='dish-name'><font size ='5'>${desserts[getRandomIndex(desserts)]}</p>
+        </div>
+     `
+    } else if (mealOption === 'meal') {
+        changeBox.innerHTML = `
+        <div class='recipe'>
+            <p class='you-should-make'>You should make:</p>
+            <p class='recipe-name'><font size ='5'>
+            ${mains[getRandomIndex(mains)]} with a side of 
+            ${sides[getRandomIndex(sides)]} and 
+            ${desserts[getRandomIndex(desserts)]}
+             for dessert!</font></p>
+        </div>
+     `
+    }
 }
 
